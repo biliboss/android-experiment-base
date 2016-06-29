abaa60730a942d84c874924b714c93980c280fe3
Gabriel Fonseca <gabriel@devmagic.com.br>
2016-06-28 23:12:53 -0300

    Project creation based on DrawerLayout

---
+++ app/src/main/java/br/com/biliboss/experimentbase/MainController.java
@@ -0,0 +1,12 @@
+package br.com.biliboss.experimentbase;
+
+/**
+ * Created by biliboss on 28/06/16.
+ */
+public class MainController {
+    private final MainActivity view;
+
+    public MainController(MainActivity view){
+        this.view = view;
+    }
+}
